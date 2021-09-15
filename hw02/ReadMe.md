b1efda5f73f1f5661565fad6053ec029c1c36183
Matthew Callahan <matthew.callahan@ieee.org>
2021-09-15 09:49:30 -0400

    all components in

--- hw02/ReadMe.md
+++ hw02/ReadMe.md
@@ -1,3 +1,9 @@
+#File structure
+ligtsAndButtons.py uses interrupts to turn on four leds based on the value of four buttons.
+
+sketch.py is the etch-a-sketch, this time using buttons to control the device
+
+
 #Measuring a gpio pin on an Oscilloscope
 
 1.using the togglegpio.sh script, I get a minimum voltage of -70mV and a maximum of 3.37V
@@ -5,11 +11,12 @@
 3. the script is using 19% cpu
 4. with a wait time of 0.01, I get 22.7Hz with period 44.1 ms. This uses 96% cpu
 5. the period jumped to 46ms when emacs was fully opened. it jumped  alot higher during oppening
-6. taking out the debugging features and comments, I get to to 48Hz
+6. taking out the debugging features and comments, I get to to 48Hz. 
 7. sh is a little bit faster, it gets 63Hz or 16ms
+Matt Hummel found that the shell script runs faster when the comments are removed
 ##python
 1. the fastest I can get is 6.4kHz, a period of 155us
-2. this uses 100%cpu
+2. this uses 100%cpu, and throttles when oppening an x window
 
 ##C
 1. without modification I can get it to go at 3.2kHz, 310us.
@@ -17,3 +24,10 @@
 3. turning xwindow off and removing lseek because it is unesscesary, I get 175us. I think this is beacuse it is actually calling sleep while my python is not.
 ##GPIOD
 I get 3.5us period and 289kHz
+
+##summerizing table
+**Script option**|Default shell|fast bash|optomised bash|optomised sh|python|C sysfs unmodified|C lseek|Csysfs fast|GPIOD
+------------
+**frequency**|4.15Hz|22.7Hz|48Hz|63Hz|6.4kHz|3.2kHz|5.13kHz|5.71kHz|289kHz
+---------
+**CPU Usage**|19%|96%|100%|100%|100%|98%|100%|100%|not measured
--- hw02/sketch.py
+++ hw02/sketch.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
 import curses
+import Adafruit_BBIO.GPIO as GPIO
+import time
  
 global board
 global x
 global y
 global width
 global height
+global stayAlive
 
 board=[]
 x,y=0,0
 width,height=0,0
 
+
+buttonUp="P9_11"
+buttonDown="P9_12"
+buttonLeft="P9_13"
+buttonRight="P9_14"
+
+buttonDict= {buttonUp:'up', buttonDown:'down',buttonLeft:'left',buttonRight:'right'}
+
 #clears screen, prints introduction message, then waits for a keypress in a blocked state
 #mainscr is the screen variable it prints to
 def printStartInfo(mainscr):
+    global stayAlive
+    stayAlive=True
     mainscr.clear()
-    mainscr.addstr("Welcome to Matthew Callahan's terminal bassed etch-a-sketch\n use the w-a-s-d keys to move the currsor around, press c to clear the screen, and x to exit\n press any key to continue")
+    mainscr.addstr("Welcome to Matthew Callahan's terminal bassed etch-a-sketch\n use the buttons to move the currsor around, press opposite buttons to clear the screen, and all of them to exit\n the application will start in five seconds")
     mainscr.refresh()
-    mainscr.getkey()
+
 #clears the screen and the variables that stores its state
 def clearScreen(mainscr):
     global board
@@ -42,13 +55,13 @@ def moveCursor(mainscr, direction):
     global width
     global height
 
-    if direction=='up':
+    if 'up' in direction:
         y=y-1;
-    elif direction=='down':
+    elif 'down' in direction:
         y=y+1;
-    elif direction=='right':
+    elif 'right' in direction:
         x=x+1;
-    elif direction=='left':
+    elif 'left' in direction:
         x=x-1;
     #since this is a digital etch-asketch, the edges wrap
     if x>=width-1:
@@ -68,31 +81,52 @@ def moveCursor(mainscr, direction):
     mainscr.addstr(boardstr)
     mainscr.move(y,x)
     mainscr.refresh()
-    
-    
+
+#this is the button intterupt handler. It checks all the buttons, checks to see if there is a command to override the buttons and make them do things other than move the cursor    
+def catchKey(key):
+    #go through eveyr button in case they are in combination, make variable to store this
+    global commands
+    commands=''
+    #for loops go through the keys
+    for button in buttonDict:
+        if GPIO.input(button):
+            commands+=buttonDict[button]
+    #check for special commands to end the program
+    if ('up' in commands) and ('down' in commands) and ('left' in commands) and ('right' in commands):
+        global keepAlive
+        keepAlive=False
+        return
+    if (('up' in commands) and ('down' in commands)) or(('left' in commands) and ('right' in commands)):
+        global screenClear
+        screenClear=True
 
 def main(mainscr):
+    global stayAlive
+    GPIO.setup(buttonUp, GPIO.IN, GPIO.PUD_DOWN)#buttons pull high so need pull-down resistors
+    GPIO.setup(buttonDown, GPIO.IN, GPIO.PUD_DOWN)#buttons pull high so need pull-down resistors
+    GPIO.setup(buttonLeft, GPIO.IN, GPIO.PUD_DOWN)#buttons pull high so need pull-down resistors
+    GPIO.setup(buttonRight, GPIO.IN, GPIO.PUD_DOWN)#buttons pull high so need pull-down resistors
+
+    #set event handlers for every button
+    GPIO.add_event_detect(buttonUp, GPIO.BOTH, callback=catchKey)
+    GPIO.add_event_detect(buttonDown, GPIO.BOTH, callback=catchKey)
+    GPIO.add_event_detect(buttonLeft, GPIO.BOTH, callback=catchKey)
+    GPIO.add_event_detect(buttonRight, GPIO.BOTH, callback=catchKey)
+
     printStartInfo(mainscr)
+    for i in range(5):
+        time.sleep(1)
     x,y=0,0
+    global screenClear
+    global commands
     clearScreen(mainscr)
-    while True:
-        s= mainscr.getkey()#wait for key press on active window, implicitly interrupt driven
-        #since only one key is accepted, this  can be a switch statement and diagonal movement is not possible
-        
-        if s=="x":
-            break #exit loop
-        elif s=="c":
+    while stayAlive:
+        if screenClear:
             clearScreen(mainscr)
-        elif s=="w":
-            moveCursor(mainscr, 'up')
-        elif s=="a":
-            moveCursor(mainscr, 'left')
-        elif s=="d":
-            moveCursor(mainscr, 'right')
-        elif s=="s":
-            moveCursor(mainscr, 'down')
-    
-        
+        if not (commands==''):
+            moveCursor(mainscr,commands)
+            commands=''
+    GPIO.cleanup()
 
         
     
