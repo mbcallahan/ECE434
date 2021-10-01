95ae2eefdf3f331b75d4e04bb8f2750297612196
Matthew Callahan <matthew.callahan@ieee.org>
2021-09-30 22:46:13 -0400

    etch-a-sketch works

--- hw04/sketch.py
+++ hw04/sketch.py
@@ -9,14 +9,8 @@ app=Flask(__name__)
 
 
 #these are state variables
-global board
-global x
-global y
-global width
-global height
-
-
 board=[]
+
 x,y=0,0
 width,height=8,8
 bus=smbus.SMBus(2)
@@ -30,10 +24,11 @@ bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
 def printStartInfo():
     allyellow=[0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff]
     bus.write_i2c_block_data(matrix,0,allyellow)
+
     
 #convert board variable to the message to send to the green leds on the dot matrix
 def printBoard():
-    global board
+    #global board
     printlist=[]
     for y in range(height):
         line=0
@@ -45,13 +40,8 @@ def printBoard():
     bus.write_i2c_block_data(matrix,0,printlist)
 #clears the screen and the variables that stores its state
 def clearScreen():
-    global screenClear
     global board
-    global x
-    global y
-    global width
-    global height
-
+    
     #clear terminal screen
     #fill array with zeros to keep track of board
     board=[[0 for i in range(width)] for j in range(height)]
@@ -62,9 +52,7 @@ def moveCursor(direction):
     global board
     global x
     global y
-    global width
-    global height
-
+    
     if 'up' in direction:
         y=y-1;
     if 'down' in direction:
@@ -86,22 +74,31 @@ def moveCursor(direction):
 
 @app.route("/")#setup the landing page, a good place to put intialization
 def index():
-    printStartInfo()
-    time.sleep(1)
-    clearScreen()
+    global board
+    global x
+    global y
+    #clearScreen()
+    print(board)
     return render_template('index.html')
 
 
 @app.route("/<deviceName>/<action>")
 def catchButtons(deviceName, action):
-    if not deviceName=="sketch":
+    if not (deviceName=="sketch"):
         print("Unexpected Button")
         return render_template('index.html')
-    if action == "clear":
+    if action == "CLEAR":
         clearScreen()
-    if action == "exit":
-        print("closing")
-        quit()
-        return render_template('index.html')
+   
+   
     moveCursor(action)
+    printBoard()
     return render_template('index.html')
+
+if __name__=="__main__": #if this is being run directly and not used as a library
+    printStartInfo()
+    time.sleep(1)
+   
+    clearScreen() #clear screen with initialization to array is correct size
+    print(board)
+    app.run(host='0.0.0.0', port=8081,debug=True)#use default ip, port 8081 to not conflict with cloud9
