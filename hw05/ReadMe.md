eaf7e772a74e0a5797361192a259f89db38ad068
Matthew Callahan <matthew.callahan@ieee.org>
2021-10-11 19:06:46 -0400

    edits made in class for hw05

--- hw05/ReadMe.md
+++ hw05/ReadMe.md
@@ -1,4 +1,4 @@
-I compiled the 5.10 version of the kernel, but it does not appear to support ethernet over USB, and I did not do much to try to debug this problem.
+I compiled the 5.10 version of the kernel and I can get it to boot up and go to standby, but it does not appear to support ethernet over USB, and I did not do much to try to debug this problem.
 
 the makeExample folder contains all the files needed from the make-file tutorial page.
 
@@ -7,4 +7,16 @@ the secondModule folder contains the second modification. It has a second switch
 
 the led folder contains the module for blinking two leds at different rates.
 
-I did not do the etch-a-sketch because figuring out how to read a file from python seemed less interesting than the kernel modules. 
+The etch-a-sketch sets itself up and runs flask to control left-right and up-down and clears when you "shake" the board by flipping it over. It has the accelerometer polling loop in a seperate task.
+
+
+I used the name adxl345 to hook it up. When it is connected, the following shows up in iio:device1
+0 dev                          0 in_accel_z_calibbias
+0 in_accel_sampling_frequency  0 in_accel_z_raw
+0 in_accel_scale               0 name
+0 in_accel_x_calibbias         0 power
+0 in_accel_x_raw               0 sampling_frequency_available
+0 in_accel_y_calibbias         0 subsystem
+0 in_accel_y_raw               0 uevent
+
+the in_accel files change when acceleration changes. 
---
+++ hw05/sketch.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+
+#i2C comes from i2cmatrix.py example from Dr. Yoder
+import smbus
+import time
+#from Dr. Yoder's presentation on gpioviaFlask, specifically app3
+from flask import Flask, render_template, request
+import threading
+
+app=Flask(__name__)
+
+
+#these are state variables
+board=[]
+
+x,y=0,0
+width,height=8,8
+bus=smbus.SMBus(2)
+matrix=0x70#fixed address for led screen
+bus.write_byte_data(matrix, 0x21, 0)   # Start oscillator (p10)
+bus.write_byte_data(matrix, 0x81, 0)   # Disp on, blink off (p11)
+bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
+
+
+#start by changing all squares to yellow to verufy led matrix is plugged in and working
+def printStartInfo():
+    allyellow=[0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff]
+    bus.write_i2c_block_data(matrix,0,allyellow)
+
+    
+#convert board variable to the message to send to the green leds on the dot matrix
+def printBoard():
+    #global board
+    printlist=[]
+    for y in range(height):
+        line=0
+        for x in range(width):
+            line|=board[x][y]<<x
+        printlist.append(line)#print in green
+        printlist.append(0)#leave red blank
+        
+    bus.write_i2c_block_data(matrix,0,printlist)
+#clears the screen and the variables that stores its state
+def clearScreen():
+    global board
+    
+    #clear terminal screen
+    #fill array with zeros to keep track of board
+    board=[[0 for i in range(width)] for j in range(height)]
+    printBoard()
+   
+#this moves the cursor on the screen and prints an X at this new location    
+def moveCursor(direction):
+    global board
+    global x
+    global y
+    
+    if 'up' in direction:
+        y=y+1;
+    if 'down' in direction:
+        y=y-1;
+    if 'right' in direction:
+        x=x+1;
+    if 'left' in direction:
+        x=x-1;
+    #since this is a digital etch-asketch, the edges wrap
+    if x>=width:
+        x=0;
+    if x<0:
+        x=width-1;
+    if y>=height:
+        y=0
+    if y<0:
+        y=height-1
+    board[y][x]=1
+
+@app.route("/")#setup the landing page, a good place to put intialization
+def index():
+    global board
+    global x
+    global y
+    #clearScreen()
+    print(board)
+    return render_template('index.html')
+
+
+@app.route("/<deviceName>/<action>")
+def catchButtons(deviceName, action):
+    if not (deviceName=="sketch"):
+        print("Unexpected Button")
+        return render_template('index.html')
+    if action == "CLEAR":
+        clearScreen()   
+    moveCursor(action)
+    printBoard()
+    return render_template('index.html')
+
+def pollAccel():
+    init1= open('/sys/class/i2c-adapter/i2c-2/new_device','w')
+    init1.write('adxl345 0x53')#this is the hardcoded address inherent in the chip
+    sensor=open('/sys/class/i2c-adapter/i2c-2/2-0053/iio:device1/in_accel_z_raw','r')#read the z axis
+    while True:
+        sensor.seek(0)#go to sof to get new value
+        temp=sensor.readline()
+        #print(temp)
+        if '-' in temp: #look for negative
+            clearScreen()
+        time.sleep(.5)
+
+if __name__=="__main__": #if this is being run directly and not used as a library
+    printStartInfo()
+    time.sleep(1)
+    
+    clearScreen() #clear screen with initialization to array is correct size
+    print(board)
+    t1=threading.Thread(target=pollAccel)
+    t1.start()#start up polling loop in parallel thread
+    app.run(host='0.0.0.0', port=8081,debug=True)#use default ip, port 8081 to not conflict with cloud9
